eb47e64ff25a877d942d97ec770354847c2d6242
Fahamin <fahamin00@gmail.com>
2024-03-25 14:10:53 +0600

    Initial commit

---
+++ app/src/main/java/kodi/tv/iptv/m3u/smarttv/OnBoardingItems.kt
@@ -0,0 +1,29 @@
+package kodi.tv.iptv.m3u.smarttv
+
+class OnBoardingItems(
+    val image: Int,
+    val title: Int,
+    val desc: Int
+) {
+    companion object {
+        fun getData(): List<OnBoardingItems> {
+            return listOf(
+                OnBoardingItems(
+                    R.drawable.in1,
+                    R.string.onBoardingTitle1,
+                    R.string.onBoardingText1
+                ),
+                OnBoardingItems(
+                    R.drawable.in2,
+                    R.string.onBoardingTitle2,
+                    R.string.onBoardingText2
+                ),
+                OnBoardingItems(
+                    R.drawable.in3,
+                    R.string.onBoardingTitle3,
+                    R.string.onBoardingText3
+                )
+            )
+        }
+    }
+}
