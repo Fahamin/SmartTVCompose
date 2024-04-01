3ac966d00772741df2392f43f8d139de6dc95378
Fahamin <fahamin00@gmail.com>
2024-04-01 13:17:10 +0600

    db change

--- app/src/main/java/kodi/tv/iptv/m3u/smarttv/db/KodiDatabase.kt
+++ app/src/main/java/kodi/tv/iptv/m3u/smarttv/db/KodiDatabase.kt
@@ -1,21 +1,19 @@
-package kodi.tv.iptv.m3u.smarttv.db;
+package kodi.tv.iptv.m3u.smarttv.db
 
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import kodi.tv.iptv.m3u.koditv.model.FavModel
+import kodi.tv.iptv.m3u.koditv.model.PlayListModel
+import kodi.tv.iptv.m3u.koditv.model.Player_FavModel
+import kodi.tv.iptv.m3u.smarttv.model.ChannelModel
 
-import kodi.tv.iptv.m3u.koditv.model.FavModel;
-import kodi.tv.iptv.m3u.koditv.model.PlayListModel;
-import kodi.tv.iptv.m3u.koditv.model.Player_FavModel;
-import kodi.tv.iptv.m3u.smarttv.model.ChannelModel;
-
-@Database(entities = {Player_FavModel.class, PlayListModel.class, FavModel.class, ChannelModel.class}, version = 6)
-public abstract class KodiDatabase extends RoomDatabase {
-    public static KodiDatabase instance;
-
-    public abstract ChannelFavDao channelFavDao();
-
-    public abstract ChannelModelDao channelModelDao();
-
-    public abstract PlayListDao playlistDao();
+@Database(
+    entities = [Player_FavModel::class, PlayListModel::class, FavModel::class, ChannelModel::class],
+    version = 6
+)
+abstract class KodiDatabase : RoomDatabase() {
+    abstract fun channelFavDao(): ChannelFavDao
+    abstract fun channelModelDao(): ChannelModelDao
+    abstract fun playlistDao(): PlayListDao
 
 }
